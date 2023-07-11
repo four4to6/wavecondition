539bc07d3d936bea823d4473e907cc229871c29a
FOUR 4to6 <four4to6@yahoo.co.jp>
2023-07-11 14:41:54 +0900

    Create wavemodelarea.js

---
+++ geojson/wavemodelarea.js
@@ -0,0 +1,43 @@
+let wavedata = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties":{"subAreaNameJP":"",
+      "areaCode":"9000",
+      "areaNameEN":"",
+      "subAreaCode":"9010",
+      "areaNameJP":"",
+      "id":"cwm_region",
+      "subAreaNameEN":""
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              120.0,
+              50.0
+            ],
+            [
+              150.0,
+              50.0
+            ],
+            [
+              150.0,
+              20.0
+            ],
+            [
+              120.0,
+              20.0
+            ],
+            [
+              120.0,
+              50.0
+            ]
+          ]
+        ]
+      }
+    }
+  ]
+}
