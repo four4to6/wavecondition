29d019e4064edada1fe8de24f3bf702705723704
FOUR 4to6 <four4to6@yahoo.co.jp>
2023-07-11 14:41:50 +0900

    Create lonlatmesh_ll10.js

---
+++ geojson/lonlatmesh_ll10.js
@@ -0,0 +1,429 @@
+let waveinf10 = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, 80], [180, 80]]
+			},
+			"properties": {"id": "lat80"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, 70], [180, 70]]
+			},
+			"properties": {"id": "lat70"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, 60], [180, 60]]
+			},
+			"properties": {"id": "lat60"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, 50], [180, 50]]
+			},
+			"properties": {"id": "lat50"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, 40], [180, 40]]
+			},
+			"properties": {"id": "lat40"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, 30], [180, 30]]
+			},
+			"properties": {"id": "lat30"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, 20], [180, 20]]
+			},
+			"properties": {"id": "lat20"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, 10], [180, 10]]
+			},
+			"properties": {"id": "lat10"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, 0], [180, 0]]
+			},
+			"properties": {"id": "lat0"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, -10], [180, -10]]
+			},
+			"properties": {"id": "lat-10"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, -20], [180, -20]]
+			},
+			"properties": {"id": "lat-20"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, -30], [180, -30]]
+			},
+			"properties": {"id": "lat-30"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, -40], [180, -40]]
+			},
+			"properties": {"id": "lat-40"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, -50], [180, -50]]
+			},
+			"properties": {"id": "lat-50"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, -60], [180, -60]]
+			},
+			"properties": {"id": "lat-60"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, -70], [180, -70]]
+			},
+			"properties": {"id": "lat-70"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, -80], [180, -80]]
+			},
+			"properties": {"id": "lat-80"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-180, 85], [-180, -85]]
+			},
+			"properties": {"id": "lon-180"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-170, 85], [-170, -85]]
+			},
+			"properties": {"id": "lon-170"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-160, 85], [-160, -85]]
+			},
+			"properties": {"id": "lon-160"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-150, 85], [-150, -85]]
+			},
+			"properties": {"id": "lon-150"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-140, 85], [-140, -85]]
+			},
+			"properties": {"id": "lon-140"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-130, 85], [-130, -85]]
+			},
+			"properties": {"id": "lon-130"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-120, 85], [-120, -85]]
+			},
+			"properties": {"id": "lon-120"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-110, 85], [-110, -85]]
+			},
+			"properties": {"id": "lon-110"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-100, 85], [-100, -85]]
+			},
+			"properties": {"id": "lon-100"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-90, 85], [-90, -85]]
+			},
+			"properties": {"id": "lon-90"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-80, 85], [-80, -85]]
+			},
+			"properties": {"id": "lon-80"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-70, 85], [-70, -85]]
+			},
+			"properties": {"id": "lon-70"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-60, 85], [-60, -85]]
+			},
+			"properties": {"id": "lon-60"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-50, 85], [-50, -85]]
+			},
+			"properties": {"id": "lon-50"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-40, 85], [-40, -85]]
+			},
+			"properties": {"id": "lon-40"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-30, 85], [-30, -85]]
+			},
+			"properties": {"id": "lon-30"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-20, 85], [-20, -85]]
+			},
+			"properties": {"id": "lon-20"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[-10, 85], [-10, -85]]
+			},
+			"properties": {"id": "lon-10"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[0, 85], [0, -85]]
+			},
+			"properties": {"id": "lon0"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[10, 85], [10, -85]]
+			},
+			"properties": {"id": "lon10"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[20, 85], [20, -85]]
+			},
+			"properties": {"id": "lon20"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[30, 85], [30, -85]]
+			},
+			"properties": {"id": "lon30"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[40, 85], [40, -85]]
+			},
+			"properties": {"id": "lon40"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[50, 85], [50, -85]]
+			},
+			"properties": {"id": "lon50"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[60, 85], [60, -85]]
+			},
+			"properties": {"id": "lon60"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[70, 85], [70, -85]]
+			},
+			"properties": {"id": "lon70"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[80, 85], [80, -85]]
+			},
+			"properties": {"id": "lon80"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[90, 85], [90, -85]]
+			},
+			"properties": {"id": "lon90"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[100, 85], [100, -85]]
+			},
+			"properties": {"id": "lon100"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[110, 85], [110, -85]]
+			},
+			"properties": {"id": "lon110"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[120, 85], [120, -85]]
+			},
+			"properties": {"id": "lon120"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[130, 85], [130, -85]]
+			},
+			"properties": {"id": "lon130"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[140, 85], [140, -85]]
+			},
+			"properties": {"id": "lon140"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[150, 85], [150, -85]]
+			},
+			"properties": {"id": "lon150"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[160, 85], [160, -85]]
+			},
+			"properties": {"id": "lon160"}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[170, 85], [170, -85]]
+			},
+			"properties": {"id": "lon170"}
+		}
+	]
+}
